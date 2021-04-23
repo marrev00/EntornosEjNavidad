7a9db7cd3d69390051cc740ef3287a407fad284b
Marcos <Marcos@DESKTOP-E9QGH1A>
2021-04-24 01:06:56 +0200

    Clase con nombre alumno y nota de examenes
    Signed-off-by: Marcos <Marcos@DESKTOP-E9QGH1A>

--- TrabajoEntorno/src/trabajoEntornos/Alumno.java
+++ TrabajoEntorno/src/trabajoEntornos/Alumno.java
@@ -3,16 +3,16 @@ package trabajoEntornos;
 public class Alumno {
 	private String nombre;
 	private int preguntasTest=30;
-	private boolean notaEx1;
-	private boolean notaEx2;
-	private boolean notaEx3;
+	private double notaEx1;
+	private double notaEx2;
+	private double notaEx3;
 	private int notaEx4;
 	private int notaEx5;
 	
 	//CONSTRUCTORES
 	public Alumno() {
 	}
-	public Alumno(String nombre, boolean ex1, boolean ex2, boolean ex3, int ex4, int ex5) {
+	public Alumno(String nombre, double ex1, double ex2, double ex3, int ex4, int ex5) {
 		this.nombre=nombre;
 		notaEx1=ex1;
 		notaEx2=ex2;
@@ -28,13 +28,13 @@ public class Alumno {
 	public String getNombre() {
 		return nombre;
 	}
-	public boolean getNotaEx1() {
+	public double getNotaEx1() {
 		return notaEx1;
 	}
-	public boolean getNotaEx2() {
+	public double getNotaEx2() {
 		return notaEx2;
 	}
-	public boolean getNotaEx3() {
+	public double getNotaEx3() {
 		return notaEx3;
 	}
 	public int getNotaEx4() {
@@ -48,14 +48,14 @@ public class Alumno {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public void setNotaEx1(boolean notaEx1) {
+	public void setNotaEx1(double notaEx1) {
 		this.notaEx1 = notaEx1;
 	}
-	public void setNotaEx2(boolean notaEx2) {
+	public void setNotaEx2(double notaEx2) {
 		this.notaEx2 = notaEx2;
 	}
 	
-	public void setNotaEx3(boolean notaEx3) {
+	public void setNotaEx3(double notaEx3) {
 		this.notaEx3 = notaEx3;
 	}
 	public void setNotaEx4(int aciertos, int fallos) {
